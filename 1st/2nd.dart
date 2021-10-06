9885f3515f6dd311485b3991d3eda20ec1de0c46
dishanttayade <dishanttayade1@outlook.com>
2021-10-06 23:33:59 +0530

    Initial Commit

---
+++ 1st/2nd.dart
@@ -0,0 +1,15 @@
+void main() {
+	var name = 'Name Of a person';
+	var year = 2000;
+	var adecimal = 634.65;
+	var flyObjects = ['1','2','Three'];
+	var json = {
+		'tags' : ['something'],
+		'url' : '/we/can/write/anything'
+	};
+	print(name);
+	print(year);
+	print(adecimal);
+	print(flyObjects);
+	print(json);
+}
